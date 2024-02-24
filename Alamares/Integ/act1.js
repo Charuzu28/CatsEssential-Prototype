5b76a0510f08eb7f8622bf72b9e5b9ab705ef32e
Charles Alamares <146437969+Charuzu28@users.noreply.github.com>
2024-02-24 23:00:43 +0800

    Add files via upload

---
+++ Alamares/Integ/act1.js
@@ -0,0 +1,12 @@
+document.write("My name is Charles Alamares");
+
+window.alert("HAAANLOOOOO");
+
+const a = "I am "
+const x = "Charles ";
+const y = "Alamares";
+const z = a + x + y;
+
+// print is in html
+
+console.log(z);
